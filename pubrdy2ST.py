c281286d580bd76e1685807771e01990dcd2a071
aforres <aforres@users.noreply.github.com>
2024-08-07 12:14:37 +1000

    Update pubrdy2ST.py

--- pubrdy2ST.py
+++ pubrdy2ST.py
@@ -1,7 +1,8 @@
 #pubrdy2ST.py ver 001 20240807
 import streamlit as st
 i = open('publishready.txt','r')
-l = unicode(i.readlines(), errors='ignore')
+l = i.readlines()
+l.encode('ascii', 'ignore')
 #str = unicode(str, errors='ignore')
 i.close()
 
